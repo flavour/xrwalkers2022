a415c9ee514667025f9745568031b798bc3ceda7
Fran Boon <fran@aidiq.com>
2022-12-07 21:47:24 +0000

    "Initial Commit"

---
+++ README.md
@@ -0,0 +1,29 @@
+# XR Walkers 2022
+
+A Map showing all the walks done by XR Walkers in 2022
+
+To join XR Walkers:
+* https://mailchi.mp/82eb3183021c/xr-walkers-landing-page
+
+## TODO
+* Show Walks on Map
+  * Have a Point for each Point
+  * Have a Line for each Line
+* Add Photos
+* Make a Quiz to match Photos to Walks
+
+## Developers
+```
+git clone https://github.com/flavour/xrwalkers2022
+cd xrwalkers2022
+npm start
+```
+
+To generate a build ready for production:
+```
+npm run build
+```
+
+Then deploy the contents of the `dist` directory to your server.
+
+You can also run `npm run serve` to serve the results of the `dist` directory for preview.
---
+++ main.js
@@ -0,0 +1,106 @@
+import './style.css';
+import {Map, View} from 'ol';
+import {defaults as defaultControls} from 'ol/control';
+import MousePosition from 'ol/control/MousePosition';
+import {createStringXY} from 'ol/coordinate';
+import GeoJSON from 'ol/format/GeoJSON';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import Overlay from 'ol/Overlay';
+import {fromLonLat} from 'ol/proj.js';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+
+const mousePositionControl = new MousePosition({
+  coordinateFormat: createStringXY(4),
+  projection: 'EPSG:4326',
+});
+
+const image = new CircleStyle({
+  radius: 5,
+  fill: new Fill({color: 'lime'}),
+  stroke: new Stroke({color: 'green', width: 3}),
+});
+
+const styles = {
+  'Point': new Style({
+    image: image,
+  }),
+  'LineString': new Style({
+    stroke: new Stroke({
+      color: 'red',
+      width: 2,
+    }),
+  }),
+}
+
+const styleFunction = function (feature) {
+  return styles[feature.getGeometry().getType()];
+};
+
+const vectorSource = new VectorSource({
+  url: 'data/walks.geojson',
+  format: new GeoJSON(),
+})
+
+const vectorLayer = new VectorLayer({
+  source: vectorSource,
+  style: styleFunction,
+});
+
+const map = new Map({
+  controls: defaultControls().extend([mousePositionControl]),
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+    vectorLayer,
+  ],
+  target: document.getElementById('map'),
+  view: new View({
+    center: fromLonLat([-1, 52]),
+    zoom: 8,
+  }),
+});
+
+const element = document.getElementById('popup');
+
+const popup = new Overlay({
+  element: element,
+  positioning: 'bottom-center',
+  stopEvent: false,
+});
+map.addOverlay(popup);
+
+let popover;
+function disposePopover() {
+  if (popover) {
+    popover.dispose();
+    popover = undefined;
+  }
+}
+// Display popup on click
+map.on('click', function (evt) {
+  const feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+    return feature;
+  });
+  disposePopover();
+  if (!feature) {
+    return;
+  }
+  popup.setPosition(evt.coordinate);
+  popover = new bootstrap.Popover(element, {
+    placement: 'top',
+    html: true,
+    content: feature.get('name'),
+  });
+  popover.show();
+});
+
+// Change mouse cursor when over marker
+map.on('pointermove', function (e) {
+  const pixel = map.getEventPixel(e.originalEvent);
+  const hit = map.hasFeatureAtPixel(pixel);
+  map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
+// Close the popup when the map is moved
+map.on('movestart', disposePopover);
